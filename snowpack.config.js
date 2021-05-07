7b16b913f13be07265b1d839b455835af6c2d089
Luca Joos <1a85ra7z@gmail.com>
2021-05-07 11:26:32 -0400

    Basic snowpack, react and tailwindcss structure

--- snowpack.config.js
+++ snowpack.config.js
@@ -4,22 +4,10 @@ module.exports = {
     public: { url: '/', static: true },
     src: { url: '/dist' },
   },
-  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
-  optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
+  plugins: ['@snowpack/plugin-postcss', '@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
+  routes: [],
+  optimize: {},
+  packageOptions: {},
+  devOptions: {},
+  buildOptions: {},
 };
