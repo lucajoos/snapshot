22fcec2b9d614e3947efef72366eb8240065cc9f
Luca Joos <1a85ra7z@gmail.com>
2021-05-07 11:17:45 -0400

    initial commit

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,11 +1,10 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    /* ... */
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
   },
-  plugins: [
-    /* ... */
-  ],
+  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
