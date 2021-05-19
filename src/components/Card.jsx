c89fe55766789c01d754a08a5619f4b6a68239cd
Luca Joos <1a85ra7z@gmail.com>
2021-05-19 10:17:56 -0400

    Add option to edit the card name

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { Clock, Edit2, PenTool, X } from 'react-feather';
+import { Check, Clock, Edit2, PenTool, X } from 'react-feather';
 import Store from '../Store';
 import { useSnapshot } from 'valtio';
 import moment from 'moment';
@@ -7,9 +7,12 @@ import moment from 'moment';
 const Card = ({ card, color='', index=-1, onClick=() => {}, innerRef=null }) => {
   const snap = useSnapshot(Store);
   const [name, setName] = useState(card?.name);
+  const [isDisabled, setIsDisabled] = useState(true);
 
   const palette = ['orange', 'pink', 'green', 'violet', 'blue'];
   const theme = useRef(color?.length === 0 ? palette[Math.floor(Math.random() * (palette.length - 1))] : color);
+
+  const inputRef = useRef(null);
 
   const handleOnClickRemove = useCallback(() => {
     let cards = snap.cards.map((card, number) => {
@@ -32,6 +35,51 @@ const Card = ({ card, color='', index=-1, onClick=() => {}, innerRef=null }) => 
     localStorage.setItem('cards', JSON.stringify({value: set}));
   }, [snap.cards, index]);
 
+  const handleOnInputChange = useCallback(event => {
+    setName(event.target.value);
+
+    let cards = snap.cards.map((card, number) => {
+      let current = Object.assign({}, card);
+
+      if(index === number) {
+        current.name = event.target.value?.length === 0 ? `Card #${localStorage.getItem('length')}` : event.target.value;
+      }
+
+      return current;
+    });
+
+    Store.cards = cards;
+    localStorage.setItem('cards', JSON.stringify({value: cards}));
+  }, []);
+
+  const handleOnClickEdit = useCallback(() => {
+    console.log(isDisabled)
+    if(isDisabled) {
+      setIsDisabled(!isDisabled);
+
+      setTimeout(() => {
+        inputRef.current?.focus();
+        inputRef.current?.select();
+      }, 50);
+    } else {
+      inputRef.current?.blur();
+    }
+  }, [isDisabled]);
+
+  const handleOnBlur = useCallback(() => {
+    if(name?.length === 0) {
+      setName(`Card #${localStorage.getItem('length')}`);
+    }
+
+    setIsDisabled(true);
+  }, [name]);
+
+  const handleOnKeyDown = useCallback((event) => {
+    if(event.keyCode === 13 || event.keyCode === 27) {
+      inputRef.current?.blur();
+    }
+  }, [])
+
   return (
     <div
       className={'my-4'}
@@ -43,7 +91,16 @@ const Card = ({ card, color='', index=-1, onClick=() => {}, innerRef=null }) => 
       >
         <div className={'grid gap-1'}>
           <div className={'grid gap-1'}>
-            <input value={card?.name} className={`text-lg font-bold bg-transparent`} type={'text'} />
+            <input
+              value={name}
+              className={`text-lg font-bold bg-transparent`}
+              type={'text'}
+              onChange={e => handleOnInputChange(e)}
+              disabled={isDisabled}
+              ref={inputRef}
+              onBlur={() => handleOnBlur()}
+              onKeyDown={(e) => handleOnKeyDown(e)}
+            />
 
             <div className={'flex items-center'}>
               <div className={'mr-1'}>
@@ -56,13 +113,19 @@ const Card = ({ card, color='', index=-1, onClick=() => {}, innerRef=null }) => 
         </div>
 
         <div className={'absolute top-0 bottom-0 m-auto right-5 items-center cursor-pointer card-remove flex'}>
-          <div className={`rounded hover:bg-${theme.current}-accent p-2 mr-1`}>
-            <Edit2 />
+          <div className={`rounded hover:bg-${theme.current}-accent p-2 mr-1`} onClick={() => {handleOnClickEdit()}}>
+            {
+              isDisabled ? <Edit2 /> : <Check/>
+            }
           </div>
 
-          <div className={`rounded hover:bg-${theme.current}-accent p-2`} onClick={() => handleOnClickRemove()}>
-            <X />
-          </div>
+          {
+            isDisabled && (
+              <div className={`rounded hover:bg-${theme.current}-accent p-2`} onClick={() => handleOnClickRemove()}>
+                <X />
+              </div>
+            )
+          }
         </div>
       </div>
     </div>
