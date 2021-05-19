f10c626857aa24a2cca195bb7d574fa673b001a3
Luca Joos <1a85ra7z@gmail.com>
2021-05-19 06:54:28 -0400

    Add button component; Refactor modules dir to components

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './modules/App.jsx';
+import App from './components/App.jsx';
 import './style.css';
 
 ReactDOM.render(
