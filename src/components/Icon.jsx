193cb7717448b2fb2f6c246e12da3f3070680198
Luca Joos <1a85ra7z@gmail.com>
2021-05-20 16:24:50 -0400

    Remove full rounding of favicons

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Icon = ({ src='#', alt='', onLoad=()=>{}}) => {
   return (
-    <div className={`inline-block rounded-full mr-1`}>
+    <div className={`inline-block rounded mr-1`}>
       <img className={'h-5'} src={src} alt={alt} onLoad={e => onLoad(e)} />
     </div>
   )
