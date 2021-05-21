6cfaa32923a2d36265488e1579ec40c4be6f6eed
Luca Joos <1a85ra7z@gmail.com>
2021-05-21 09:11:55 -0400

    Improve icons list design

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Icon = ({ src='#', alt='', onLoad=()=>{}}) => {
   return (
     <div className={`inline-block rounded mr-1`}>
-      <img className={'h-5'} src={src} alt={alt} onLoad={e => onLoad(e)} />
+      <img className={'h-4'} src={src} alt={alt} onLoad={e => onLoad(e)} />
     </div>
   )
 };
