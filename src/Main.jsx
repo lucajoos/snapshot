a9c8e30859654bc3ba7bcec3067b4e07f7210c0d
Luca Joos <1a85ra7z@gmail.com>
2022-02-10 16:43:31 +0100

    Rename main.jsx to Main.jsx; Move styles.css into separate folder

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-import './style.css';
+import './styles/style.css';
 
 ReactDOM.render(
   <React.StrictMode>
