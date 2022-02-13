89565e0927e67e959ad25e0bad086b0082eb1f39
Luca Joos <1a85ra7z@gmail.com>
2022-02-13 22:04:28 +0100

    Update setting icons; Add general settings functionality

--- src/components/Modal/Content/Settings/Category/General.jsx
+++ src/components/Modal/Content/Settings/Category/General.jsx
@@ -7,7 +7,7 @@ import {
   CornerLeftDown,
   CornerRightDown,
   CornerRightUp,
-  Download, Rewind,
+  Download, Maximize, Rewind,
   Share,
   Trash,
   Upload,
@@ -16,25 +16,19 @@ import {
 } from 'react-feather';
 
 const General = () => {
-  const handleOnClickOpenFullscreen = useCallback(async () => {
+  const importRef = useRef(null);
+
+  const handleOnClickFullscreen = useCallback(async () => {
     await helpers.api.do('tabs.create', {
       url: await helpers.api.do('runtime.getURL', 'app.html?fullscreen=true')
     }, { isWaiting: false });
   }, []);
 
-  const handleOnClickAboutLicenses = useCallback(async () => {
-    await helpers.api.do('tabs.create', {
-      url: await helpers.api.do('runtime.getURL', 'licenses.html')
-    }, { isWaiting: false });
+  const handleOnClickImport = useCallback(() => {
+    importRef.current?.click();
   }, []);
 
-  const advancedImportInputRef = useRef(null);
-
-  const handleOnClickAdvancedImport = useCallback(() => {
-    advancedImportInputRef.current?.click();
-  }, []);
-
-  const handleOnChangeAdvancedImportInput = useCallback(async event => {
+  const handleOnChangeImport = useCallback(async event => {
     event.preventDefault();
 
     if(event.target?.files?.length > 0) {
@@ -43,28 +37,36 @@ const General = () => {
     }
   }, [])
 
-  const handleOnClickAdvancedExport = useCallback(() => {
+  const handleOnClickExport = useCallback(() => {
     helpers.cards.export();
   }, []);
 
-  const handleOnCLickAdvancedReset = useCallback(async () => {
+  const handleOnClickReset = useCallback(async () => {
     localStorage.clear();
     await helpers.api.do('window.close');
   }, []);
 
+  const handleOnClickAboutLicenses = useCallback(async () => {
+    await helpers.api.do('tabs.create', {
+      url: await helpers.api.do('runtime.getURL', 'licenses.html')
+    }, { isWaiting: false });
+  }, []);
+
   return (
     <div className={'flex flex-col gap-6'}>
+      <Option.Category title={'Open Fullscreen'} icon={<Maximize />} onClick={() => handleOnClickFullscreen()}/>
+
       <div>
         <Section>Advanced</Section>
 
-        <Option.Category title={'Import Data'} icon={<Download />}/>
+        <Option.Category title={'Import Data'} icon={<Download />} onClick={() => handleOnClickImport()}/>
+        <input className={ 'hidden' } type={ 'file' } onChange={ event => handleOnChangeImport(event) } ref={importRef} />
 
-        <Option.Category title={'Export Data'} icon={<Upload />}/>
-        <input className={ 'hidden' } type={ 'file' } onChange={ event => handleOnChangeAdvancedImportInput(event) } ref={advancedImportInputRef} />
+        <Option.Category title={'Export Data'} icon={<Upload />} onClick={() => handleOnClickExport()}/>
 
         <hr className={'my-2'}/>
 
-        <Option.Category title={'Reset'} icon={<Rewind />}/>
+        <Option.Category title={'Reset'} icon={<Rewind />} onClick={() => handleOnClickReset()}/>
       </div>
 
       <div>
--- src/components/Modal/Content/Settings/Category/Sync.jsx
+++ src/components/Modal/Content/Settings/Category/Sync.jsx
@@ -3,14 +3,14 @@ import Switch from '../../../../Input/Switch';
 import { useSnapshot } from 'valtio';
 import Store from '../../../../../Store';
 import { Button, Link, Option } from '../../../../Base';
-import { HardDrive, Maximize, RefreshCw, Settings, Tool, Trash } from 'react-feather';
+import { HardDrive, Layers, Maximize, RefreshCw, Settings, Sliders, Tool, Trash } from 'react-feather';
 
 const Sync = () => {
   const snap = useSnapshot(Store)
   return (
     <div className={'flex flex-col gap-2'}>
-      <Option.Switch title={'Update Cards'} icon={<HardDrive />}/>
-      <Option.Switch title={'Update Settings'} icon={<Tool />}/>
+      <Option.Switch title={'Synchronize Cards'} icon={<Layers />}/>
+      <Option.Switch title={'Synchronize Settings'} icon={<Settings />}/>
     </div>
   )
 };
