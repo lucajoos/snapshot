ebed0c3d505c67960e4bb80226b40c2f6b0b41e1
Luca Joos <1a85ra7z@gmail.com>
2022-02-13 21:52:48 +0100

    Update category switch component

--- src/components/Base/Option/Switch.jsx
+++ src/components/Base/Option/Switch.jsx
@@ -2,16 +2,15 @@ import { ChevronRight } from 'react-feather';
 import React from 'react';
 import { Switch } from '../../Input';
 
-const Category = ({ title, icon=null, onClick=()=>{}, className=''}) => {
+const Category = ({ title, icon=null, value=true, onChange=()=>{}, className=''}) => {
   return (
     <div
-      className={`option cursor-pointer flex items-center width-full rounded pl-4 pr-2 py-3 justify-between${className.length > 0 ? ` ${className}` : ''}`}
-      onClick={() => onClick(title)}>
+      className={`option flex items-center width-full rounded pl-4 pr-2 py-3 justify-between${className.length > 0 ? ` ${className}` : ''}`}>
       <div className={'flex gap-4 items-center'}>
         {icon}
         <p>{title}</p>
       </div>
-      <Switch />
+      <Switch value={value} onChange={() => onChange()} />
     </div>
   )
 };
