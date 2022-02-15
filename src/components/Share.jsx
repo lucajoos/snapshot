fc5e918fceefdda5e8d742a158678ba62e72abd1
Luca Joos <1a85ra7z@gmail.com>
2022-02-15 22:17:25 +0100

    Re-design share screen

--- src/components/Share.jsx
+++ src/components/Share.jsx
@@ -1,6 +1,7 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { Header } from './Base';
 import { useCallback, useEffect, useState } from 'react';
+import { Briefcase, Globe, Home, Layers } from 'react-feather';
 
 const Share = () => {
   const params = useParams();
@@ -35,17 +36,32 @@ const Share = () => {
   }, []);
 
   return (
-    <div className={'flex justify-center gap-6 items-center h-full'}>
-      <div className={'p-10 bg-gray-200 rounded cursor-pointer'} onClick={() => handleOnClickWeb()}>
-        <Header>Open Here</Header>
-      </div>
-      {
-        isExtensionInstalled && (
-          <div className={'p-10 bg-gray-200 rounded cursor-pointer'} onClick={() => handleOnClickExtension()}>
-            <Header>Extension</Header>
+    <div className={'flex flex-col h-full'}>
+      <Header className={'ml-10 mt-10'}>
+        <img src='../icons/png/128x128.png' className={'h-12'} />
+        <span>Snapshot</span>
+      </Header>
+      <div className={'flex flex-col justify-center items-center gap-8 h-full mb-16'}>
+        <p className={'text-xl'}>Choose Application</p>
+        <div className={'flex gap-10'}>
+          <div className={'p-10 border-4 border-gray-200 rounded cursor-pointer w-[250px] max-w-[30vw]'} onClick={() => handleOnClickWeb()}>
+            <div className={'flex flex-col gap-2 items-center'}>
+              <Globe size={32}/>
+              <Header>Browser</Header>
+            </div>
           </div>
-        )
-      }
+          {
+            isExtensionInstalled && (
+              <div className={'p-10 border-4 border-gray-200 rounded cursor-pointer w-[250px] max-w-[30vw]'} onClick={() => handleOnClickExtension()}>
+                <div className={'flex flex-col gap-2 items-center'}>
+                  <Layers size={32}/>
+                  <Header>Extension</Header>
+                </div>
+              </div>
+            )
+          }
+        </div>
+      </div>
     </div>
   )
 };
