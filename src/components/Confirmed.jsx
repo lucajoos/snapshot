cf9784d411a26a7957a3d3b58c1cfc0a6d54a79c
Luca Joos <1a85ra7z@gmail.com>
2022-02-16 11:49:50 +0100

    Re-design email confirmed screen

--- src/components/Confirmed.jsx
+++ src/components/Confirmed.jsx
@@ -1,13 +1,15 @@
 import { Header } from './Base';
+import { Mail } from 'react-feather';
 
 const Confirmed = () => {
   return (
     <div className={'flex flex-col h-full'}>
-      <Header className={'ml-10 mt-10'}>
+      <Header className={'ml-10 mt-10 pointer-events-none'}>
         <img src='../icons/png/128x128.png' className={'h-12'}  alt={'Icon'}/>
         <span>Snapshot</span>
       </Header>
-      <div className={'flex flex-col justify-center items-center h-full mb-16'}>
+      <div className={'flex flex-col justify-center items-center h-full mb-16 gap-4'}>
+        <Mail size={32}/>
         <p className={'text-xl'}>Your email has been successfully confirmed.</p>
       </div>
     </div>
