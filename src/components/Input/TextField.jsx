9aaa796360f2359826b14b7624bef83663944868
Luca Joos <1a85ra7z@gmail.com>
2022-04-10 14:23:35 +0200

    [ref] Disable SpellChecking by Default

--- src/components/Input/TextField.jsx
+++ src/components/Input/TextField.jsx
@@ -1,6 +1,6 @@
 import React  from 'react';
 
-const TextField = ({ type='text', placeholder='', value='', icon=null, ability=null, onClickAbility=()=>{}, onChange=()=>{}, onKeyDown=()=>{}, nativeRef, className='' }) => {
+const TextField = ({ type='text', placeholder='', value='', icon=null, ability=null, isSpellChecking=false, onClickAbility=()=>{}, onChange=()=>{}, onKeyDown=()=>{}, nativeRef, className='' }) => {
   return (
     <div className={`input border-b-2 border-gray-300 transition-all py-3 w-full${className.length > 0 ? ` ${className}` : ''}`}>
       <label className={'flex items-center mx-1'}>
@@ -17,6 +17,7 @@ const TextField = ({ type='text', placeholder='', value='', icon=null, ability=n
           ref={nativeRef}
           className={`${icon ? 'ml-2 ' : ''}${ability ? 'mr-2 ' : ''}text-text-default w-full placeholder-text-default focus:border-text-default`}
           onKeyDown={event => onKeyDown(event)}
+          spellCheck={isSpellChecking}
         />
         {ability && (
           <div className={'text-gray-500 cursor-pointer mr-2'} onClick={() => onClickAbility()}>
