fb3e9e2df551e11fe7d95e22b20f5aec4aec6714
Luca Joos <1a85ra7z@gmail.com>
2022-04-12 18:06:51 +0200

    [ref] Semicolons in Deno

--- functions/index.ts
+++ functions/index.ts
@@ -6,18 +6,18 @@ await serve(async (req: Request) => {
     if (req.method === 'OPTIONS') {
         return new Response('ok', {
             headers: cors
-        })
+        });
     }
 
     let response = new Response('ok', {
         headers: cors
-    })
+    });
 
     switch (`/${req.url.split('/').slice(3).join('/')}`) {
         case '/url':
-            response = await url(req)
-            break
+            response = await url(req);
+            break;
     }
 
-    return response
-})
+    return response;
+});
