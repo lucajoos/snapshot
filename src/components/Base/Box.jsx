23737fde1327815325450a8c3cf4de59a470bb54
Luca Joos <1a85ra7z@gmail.com>
2022-04-14 13:05:11 +0200

    [ref] Update border with of Box

--- src/components/Base/Box.jsx
+++ src/components/Base/Box.jsx
@@ -2,7 +2,7 @@ import { Header } from './index';
 
 const Box = ({ children, icon=null, onClick=()=>{} }) => {
   return (
-    <div className={'p-10 border-4 border-gray-200 rounded cursor-pointer w-[250px]'} onClick={() => onClick()}>
+    <div className={'p-10 border border-gray-200 rounded cursor-pointer w-[250px]'} onClick={() => onClick()}>
       <div className={'flex flex-col gap-2 items-center'}>
         {icon}
         <Header>{children}</Header>
