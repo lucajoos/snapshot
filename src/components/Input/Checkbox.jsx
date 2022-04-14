810d62d6d7e9eb72bb9a8e3c95f43e5fc44cfb23
Luca Joos <1a85ra7z@gmail.com>
2022-04-14 13:37:12 +0200

    [ref] Decrease border size of Checkbox

--- src/components/Input/Checkbox.jsx
+++ src/components/Input/Checkbox.jsx
@@ -7,7 +7,7 @@ const Checkbox = ({ value=true, isEnabled=true, onChange=()=>{} }) => {
       <input type={'checkbox'} onChange={e => onChange(e)} defaultChecked={value} className={'hidden'} disabled={!isEnabled} />
 
       <div
-        className={`w-6 h-6 mr-1 cursor-pointer transition-all rounded border-2 border-gray-300 ${!isEnabled ? 'cursor-not-allowed' : ''}`}
+        className={`w-6 h-6 mr-1 cursor-pointer transition-all rounded border border-gray-300 ${!isEnabled ? 'cursor-not-allowed' : ''}`}
       >
         <div className={`grid justify-center items-center h-full duration-50 transition-all ${!!value ? 'opacity-100' : 'opacity-0'}`}>
           <Check size={20} />
